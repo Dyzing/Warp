d15b5fd8dd918a1a23d3c950e1f2d6402c934eaf
vsteenbr <73848892+vsteenbr@users.noreply.github.com>
2021-10-24 23:27:03 -0400

    conseiled et ecouteed complete

--- src/com/company/ConseilED.java
+++ src/com/company/ConseilED.java
@@ -14,14 +14,41 @@ public abstract class ConseilED extends Service implements ConseilInterface {
         switch (random_value) {
             case 1:
                 System.out.println(Constantes.CONSEILED1);
+                break;
             case 2:
                 System.out.println(Constantes.CONSEILED2);
+                break;
             case 3:
                 System.out.println(Constantes.CONSEILED3);
+                break;
+            default:
+                break;
         }
     }
 
     @Override
     public void conseilFonctionDemande() {
+<<<<<<< Updated upstream
+=======
+
+        Random random = new Random();
+        int random_value = random.nextInt(3) + 1; switch (random_value) {
+            case 1:
+                System.out.println(Constantes.DEMANDEED1);
+                System.out.println(Constantes.CONSEILED1);
+                break;
+            case 2:
+                System.out.println(Constantes.DEMANDEED2);
+                System.out.println(Constantes.CONSEILED2);
+                break;
+            case 3:
+                System.out.println(Constantes.DEMANDEED3);
+                System.out.println(Constantes.CONSEILED3);
+                break;
+            default:
+                break;
+        }
+
+>>>>>>> Stashed changes
     }
 }
--- src/com/company/Constantes.java
+++ src/com/company/Constantes.java
@@ -6,7 +6,7 @@ public class Constantes {
     public static final String CONSEILED2 = "Psykers je te conseille de faire la paix avec tes ennemis";
     public static final String CONSEILED3 = "Psykers je te conseille de t'ailler avec tes voisins";
 
-    public static final String ECOUTEED = "L'Empereur-Dieu a ecouté";
+    public static final String ECOUTEED = "L'Empereur-Dieu a ecouté mais ne fait rien";
 
     public static final String CATACLYSMEED1 = "Super Nova destructice ";
     public static final String CATACLYSMEED2 = "Eruption solaire";
@@ -18,10 +18,14 @@ public class Constantes {
     public static final String DEMANDEED4 = "Oh grand Empereur-Dieu, fait une Super Nova destructice !";
     public static final String DEMANDEED5 = "Oh grand Empereur-Dieu, fait une Eruption Solaire !";
     public static final String DEMANDEED6 = "Oh grand Empereur-Dieu, fait une pluie d'asteroïdes!";
+    public static final String DEMANDEED7 = "Oh grand Empereur-Dieu, soigne mes combattants";
+    public static final String DEMANDEED8 = "Oh grand Empereur-Dieu, donne nous de la force pour le combat";
+    public static final String DEMANDEED9 = "Oh grand Empereur-Dieu, donne nous de l'argent";
+
 
 
 
     public static final String MIRACLEED1 = "Soigne tous les Psykers au combat";
     public static final String MIRACLEED2 = "Donne de la force tous les Psykers au combat";
-    public static final String MIRACLEED3 = "Enrichie tous les Psykers au combat";
+    public static final String MIRACLEED3 = "Enrichis tous les Psykers";
 }
