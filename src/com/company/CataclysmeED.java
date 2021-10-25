b9b7cb1502dded85abf218344b4061903ed5fe05
Dyzing <guillaume.trem@outlook.fr>
2021-10-25 18:22:06 -0400

    Affichage main propre

--- src/com/company/CataclysmeED.java
+++ src/com/company/CataclysmeED.java
@@ -11,15 +11,15 @@ public class CataclysmeED extends Service implements  CataclysmeInterface{
 
         switch (random_value) {
             case 1:
-                System.out.print("Empereur-Dieu : ");
+                System.out.print("-Empereur-Dieu : ");
                 System.out.println(Constantes.CATACLYSMEED1);
                 break;
             case 2:
-                System.out.print("Empereur-Dieu : ");
+                System.out.print("-Empereur-Dieu : ");
                 System.out.println(Constantes.CATACLYSMEED2);
                 break;
             case 3:
-                System.out.print("Empereur-Dieu : ");
+                System.out.print("-Empereur-Dieu : ");
                 System.out.println(Constantes.CATACLYSMEED3);
                 break;
             default:
@@ -35,17 +35,17 @@ public class CataclysmeED extends Service implements  CataclysmeInterface{
         switch (random_value) {
             case 1:
                 System.out.println(Constantes.DEMANDEED4);
-                System.out.print("Empereur-Dieu : ");
+                System.out.print("-Empereur-Dieu : ");
                 System.out.println(Constantes.CATACLYSMEED1);
                 break;
             case 2:
                 System.out.println(Constantes.DEMANDEED5);
-                System.out.print("Empereur-Dieu : ");
+                System.out.print("-Empereur-Dieu : ");
                 System.out.println(Constantes.CATACLYSMEED2);
                 break;
             case 3:
                 System.out.println(Constantes.DEMANDEED6);
-                System.out.print("Empereur-Dieu : ");
+                System.out.print("-Empereur-Dieu : ");
                 System.out.println(Constantes.CATACLYSMEED3);
                 break;
             default:
