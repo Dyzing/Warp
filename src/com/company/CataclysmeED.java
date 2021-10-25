610818a4a8b5682beba4a203ec209be3d39db816
Dyzing <guillaume.trem@outlook.fr>
2021-10-24 22:51:47 -0400

    WIP : CataclysmeED

--- src/com/company/CataclysmeED.java
+++ src/com/company/CataclysmeED.java
@@ -1,4 +1,31 @@
 package com.company;
 
-public class CataclysmeED {
+import java.util.Random;
+
+public abstract class CataclysmeED implements  CataclysmeInterface{
+
+    @Override
+    public void RealiserCataclysmeSansRaison() {
+        Random random = new Random();
+        int random_value = random.nextInt(3) + 1;
+
+        switch (random_value) {
+            case 1:
+                System.out.println(Constantes.MIRACLEED1);
+            case 2:
+                System.out.println(Constantes.MIRACLEED2);
+            case 3:
+                System.out.println(Constantes.MIRACLEED3);
+        }
+    }
+
+    @Override
+    public void RealiserCataclysmeFonctionDemande() {
+
+    }
+
+    @Override
+    public void RealiserCataclysmeEtMiracle() {
+
+    }
 }
--- src/com/company/CataclysmeInterface.java
+++ src/com/company/CataclysmeInterface.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public interface CataclysmeInterface {
+
+    public abstract void RealiserCataclysmeSansRaison();
+
+    public abstract void RealiserCataclysmeFonctionDemande();
+
+    public abstract void RealiserCataclysmeEtMiracle();
 }
